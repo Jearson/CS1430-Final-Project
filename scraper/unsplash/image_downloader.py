8e86cfc8555191486cd63a298c192d55344f3b78
Jearson <jearson_alfajardo@brown.edu>
2019-04-24 19:29:54 -0400

    Okay now different queries produce different names for image files

--- scraper/unsplash/image_downloader.py
+++ scraper/unsplash/image_downloader.py
@@ -5,7 +5,8 @@ import urllib.request as urlr
 if len(sys.argv) < 2:
 	sys.exit("Give me a query!")
 
-json_url = "https://unsplash.com/napi/search/photos?query={}&xp=&per_page=30&page={}".format(sys.argv[1],0)
+query=sys.argv[1]
+json_url = "https://unsplash.com/napi/search/photos?query={}&xp=&per_page=30&page={}".format(query,0)
 jdict = json.loads(urlr.urlopen(json_url).read().decode())
 num_pgs=jdict["total_pages"]
 
@@ -18,4 +19,4 @@ for p in range(num_pgs):
 	print("Downloading {} images from page {}".format(len(res),p))
 	for i in range(len(res)):
 		img_url = res[i]["urls"]["raw"]
-		urlr.urlretrieve(img_url, "imgs/macbook_{}-{}.png".format(p,i))
+		urlr.urlretrieve(img_url, "imgs/{}_{}-{}.png".format(query,p,i))
