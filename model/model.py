ee0bf1d42e0bae6191fef6a94714cc79a9c69da2
TiffL <lei.tiffany@gmail.com>
2019-05-06 13:05:48 -0400

    Updated model & added bounding boxes code

--- model/model.py
+++ model/model.py
@@ -5,6 +5,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io, img_as_ubyte, img_as_float32
+import skimage.segmentation
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 class Model:
@@ -21,6 +23,7 @@ class Model:
         self.loss = self.loss_function()
         self.optimize = self.optimizer_function()
         self.accuracy = self.accuracy_function()
+        self.answer = self.getAnswer()
     
     def forward_pass(self):
         
@@ -40,72 +43,184 @@ class Model:
                              
         W1 = tf.Variable(tf.truncated_normal([372736, 256], stddev=0.1))
         b1 = tf.Variable(tf.truncated_normal([256], stddev=0.1))
-        W2 = tf.Variable(tf.truncated_normal([256, 10], stddev=0.1))
-        b2 = tf.Variable(tf.truncated_normal([10], stddev=0.1))
+        
+        W2 = tf.Variable(tf.truncated_normal([256, 2048], stddev=0.1))
+        b2 = tf.Variable(tf.truncated_normal([2048], stddev=0.1))
+        
+        W3 = tf.Variable(tf.truncated_normal([2048, 10], stddev=0.1))
+        b3 = tf.Variable(tf.truncated_normal([10], stddev=0.1))
                              
         feedForward1 = tf.nn.relu(tf.matmul(convReshaped, W1) + b1)
+        feedForward2 = tf.nn.relu(tf.matmul(feedForward1, W2) + b2)
                              
-        return tf.nn.softmax(tf.matmul(feedForward1, W2) + b2)
+        return tf.matmul(feedForward2, W3) + b3 #tf.nn.softmax(tf.matmul(feedForward1, W2) + b2)
     
     def loss_function(self):
-        return tf.reduce_mean(-tf.reduce_sum(self.label * tf.log(tf.clip_by_value(self.prediction,1e-10,1.0)), reduction_indices=[1]))
+        return tf.losses.softmax_cross_entropy(self.label, self.prediction)
     
     def optimizer_function(self):
         return tf.train.AdamOptimizer(1E-4).minimize(self.loss)
     
     def accuracy_function(self):
-        print("actual")
-        print(tf.argmax(self.prediction, 1))
-        print("Correct")
-        print(tf.argmax(self.label, 1))
         correct_prediction = tf.equal(tf.argmax(self.prediction, 1),
                                       tf.argmax(self.label, 1))
         return tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+    def getAnswer(self):
+        return tf.argmax(self.prediction, 1)
+
+# read in zipped files
 def readZippedFiles(pathList):
     allUnzippedImgs = []
     for path in pathList:
         # Read in zipped images without extracting:
         zippedF = zipfile.ZipFile(path, 'r')
         zippedNames = zippedF.namelist()
-    
-        for currFile in zippedNames[:len(zippedNames)-1]:
+        
+        for i, currFile in enumerate(zippedNames[:len(zippedNames)-1]):
+            printRes = str(i) + ") " + currFile
+            print(printRes)
             byteFile = zippedF.read(currFile)
             imgFile = cv2.imdecode(np.frombuffer(byteFile, np.uint8), cv2.IMREAD_COLOR) # 1 for greyscale?
             img = img_as_float32(imgFile)
             allUnzippedImgs.append(img)
         print(img.shape)
-#plt.imshow(img)
-#plt.show()
+        #plt.imshow(img)
+        #plt.show()
         #break
     return allUnzippedImgs
+
+# read in unzipped training data + apply labels
+def readFiles(pathList):
+    allUnzippedImgs = []
+    labels = []
+    for i, path in enumerate(pathList):
+        for dir, subdir, files in os.walk(path):
+            for f in files:
+                imgPath = os.path.join(dir, f)
+                with open(imgPath, "rb") as opened:
+                    temp = opened.read()
+                    byteFile = bytearray(temp)
+                    imgFile = cv2.imdecode(np.frombuffer(byteFile, np.uint8), cv2.IMREAD_COLOR) # 1 for greyscale?
+                    img = img_as_float32(imgFile)
+                    resizedImg = cv2.resize(img, dsize=(448, 416), interpolation=cv2.INTER_CUBIC)
+                    allUnzippedImgs.append(resizedImg)
+                    if i == 0: # Macbook
+                        labels.append([1., 0., 0., 0., 0., 0., 0., 0., 0., 0.]) #0
+                    elif i == 1: # Phone
+                        labels.append([0., 0., 0., 0., 1., 0., 0., 0., 0., 0.]) #4
+                    elif i == 2: # Camera
+                        labels.append([0., 0., 0., 0., 0., 0., 0., 1., 0., 0.]) #7
+    return labels, allUnzippedImgs
+
+# read in testing data
+def getTestImgs(pathList):
+    allUnzippedImgs = []
+    for i, path in enumerate(pathList):
+        for dir, subdir, files in os.walk(path):
+            for f in files:
+                imgPath = os.path.join(dir, f)
+                print(imgPath)
+                with open(imgPath, "rb") as opened:
+                    '''
+                    temp = opened.read()
+                    byteFile = bytearray(temp)
+                    imgFile = cv2.imdecode(np.frombuffer(byteFile, np.uint8), cv2.IMREAD_COLOR) # 1 for greyscale?
+                    img = img_as_float32(imgFile)
+                    resizedImg = cv2.resize(img, dsize=(448, 416), interpolation=cv2.INTER_CUBIC)
+                    '''
+                    img = cv2.imread(imgPath,cv2.IMREAD_COLOR)
+                    resizedImg = cv2.resize(img, dsize=(448, 416), interpolation=cv2.INTER_CUBIC)
+                    allUnzippedImgs.append(resizedImg)
+    return allUnzippedImgs
+
+# get bounding boxes
+def getBoundingBoxes(img, numRequested):
+    segment_mask = skimage.segmentation.felzenszwalb(np.uint8(img), scale=100, sigma=0.8, min_size=50)
+    numSegments = segment_mask.max()
+    boundingBoxes = []
+    sizes = []
+    #largestSize, secondLargest = 0, 0
+    for seg in range(numSegments):
+        top, bottom, left, right, size = None, None, None, None, None
+        for i in range(len(segment_mask)):
+            for j in range(len(segment_mask[0])):
+                curr = segment_mask[i][j]
+                if curr == seg: #mode:
+                    if top == None:
+                        top = i
+                        bottom = i
+                        left = j
+                        right = j
+                    top = min(i, top)
+                    bottom = max(i, bottom)
+                    left = min(j, left)
+                    right = max(j, right)
+        size = (right - left) * (bottom - top)
+        '''
+        if (size > largestSize):
+        secondLargest = largestSize
+        largestSize = size
+        elif (size > secondLargest):
+        secondLargest = size
+        '''
+        sizes.append(size)
+        boundingBoxes.append([top, bottom, left, right, size])
+
+    sizes.sort(reverse=True)
+
+    # show bounding boxes for debugging purposes
+    fig = plt.figure(figsize=(12, 5))
+    ax1 = fig.add_subplot(121)
+    for [top, bottom, left, right, size] in boundingBoxes:
+        if (size in sizes[0:numRequested]):
+            rectangle = matplotlib.patches.Rectangle((left, top), (right-left), (bottom-top), fill=False, edgecolor='red', linewidth=3)
+            ax1.add_patch(rectangle)
+    ax1.imshow(segment_mask);
+    plt.tight_layout()
+    plt.show()
+
+# translate results to english
+def translateResults(resArr):
+    translations = []
+    for result in resArr:
+        if result == 0:
+            translations.append("Mac Laptop")
+        elif result == 1:
+            translations.append("PC Laptop")
+        elif result == 2:
+            translations.append("Desktop")
+        elif result == 3:
+            translations.append("Tablet")
+        elif result == 4:
+            translations.append("Phone")
+        elif result == 5:
+            translations.append("Router")
+        elif result == 6:
+            translations.append("Smart Speaker")
+        elif result == 7:
+            translations.append("Camera")
+        elif result == 8:
+            translations.append("TV")
+        elif result == 9:
+            translations.append("Handheld Game Console")
+    return translations
 
 def main():
     # number of label categories: 10
     numLabels = 10 # labels should be one-hot vectors
     
     #import data
-    pathList = ['../data/coil-20-unproc.zip'] # list of zipped data
-    imgData = np.array(readZippedFiles(pathList))
-    print(imgData.shape)
+    pathList = ['../scraper/unsplash/imgs/Macbook/', '../scraper/unsplash/imgs/Phone/', '../scraper/unsplash/imgs/Camera/'] # list of zipped data
+    labelData, imgData = readFiles(pathList)
+    labelData = np.array(labelData)
+    imgData = np.array(imgData)
     imgHeight = imgData.shape[1]
     imgWidth = imgData.shape[2]
-    
-    labelData = np.zeros((imgData.shape[0], numLabels)) # should be of size: imgData.shape[0] x numLabels <-- due to one hot vector
-    #reshapedImgData = np.reshape(imgData, (imgData.shape[0], imgHeight * imgWidth))
-    
-    for i in range(len(labelData)):
-        res = int(i / 72)
-        if res == 0:
-            labelData[i][0] = 1
-        elif res == 1:
-            labelData[i][1] = 1
-        elif res == 2:
-            labelData[i][2] == 1
-        elif res == 2:
-            labelData[i][3] == 1
-        elif res == 2:
-            labelData[i][4] == 1
+    print("LABEL DATA")
+    print(labelData.shape)
+    print("IMAGE DATA")
+    print(imgData.shape)
     
     # Model Initialization:
     
@@ -114,37 +229,44 @@ def main():
     session.run(tf.global_variables_initializer())
     
     # Training:
-    numEpochs = 10
+    numEpochs = 12
     batchSz = 50
     numTrainData = imgData.shape[0] # amount of images for training
-    
+
+    # check input data
+    # print(imgData)
+    # for i in range(len(imgData[0])):
+    #     for j in range(len(imgData[0][i])):
+    #         print(imgData[0][i][j])
+    print(labelData)
+
     for epoch in range(numEpochs):
-        '''
-        currLoss, _, acc = session.run([m.loss, m.optimize, m.accuracy],
-                                       feed_dict={m.image: imgData, m.label: labelData})
-        print("CURRLOSS")
-        print(currLoss)
-        print(acc)
-        '''
+        print("============ EPOCH ============")
+        totalLoss = 0
+        totalChecked = 0
+        totalAcc = 0
         for start, end in zip(range(0, numTrainData - batchSz, batchSz),
                               range(batchSz, numTrainData, batchSz)):
             currLoss, _, acc = session.run([m.loss, m.optimize, m.accuracy],
                                       feed_dict={m.image: imgData[start:end], m.label: labelData[start:end]})
-                                      #print("CURRLOSS")
-                                      #print(currLoss)
-#print(acc)
+                                      
+            #print("CURRLOSS")
+            #print(currLoss)
+            #print(acc)
 
-            
-
+            totalLoss += currLoss
+            totalChecked += batchSz
+            totalAcc += (acc * batchSz)
+        print("BATCH LOSS")
+        print(totalLoss / totalChecked)
+        print("BATCH ACC")
+        print(totalAcc / totalChecked)
 
     # Testing:
-    '''
-    testImages = []
-    testLabels = []
-    
-    avgAccuracy = session.run(m.accuracy, feed_dict={m.image: imgData, m.label: labelData})
-    print(avgAccuracy)
-    '''
+    testPathList = ['../testData/']
+    testImgData = np.array(getTestImgs(testPathList))
+    result = session.run(m.answer, feed_dict={m.image: testImgData})
+    print(translateResults(result))
 
     return
 
